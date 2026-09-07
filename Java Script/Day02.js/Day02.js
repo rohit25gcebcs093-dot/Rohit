fcb8962df3cd4b2ebf02ec75a46249fa0455ddcd
Rohit Maddheshiya <rohit.25gcebcs093@galgotiacollege.edu>
2026-09-07 18:25:30 +0530

    Day 02 of JS

---
+++ Java Script/Day02.js/Day02.js
@@ -0,0 +1,44 @@
+// Operators
+//1.Arithmetic
+
+console.log(2+5);
+console.log(2-5);
+console.log(2*5);
+console.log(6/2);
+console.log(5%2);
+console.log(5**2);
+
+//Assignment
+let y=10;
+let x=20;
+x=x+y;
+console.log(x);
+
+//Comparison
+let a=20;
+let b=10;
+console.log(a>=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+
+let c=20;
+let d="20";
+console.log(c==d);
+
+let e="121ac";
+let f=Number(e);
+console.log(f);
+console.log(0/0);
+console.log(typeof f);
+
+let g=10;
+let h=String(g);
+console.log(h);
+console.log(typeof h);
+
+console.log("Rohit">"Mohit");
+console.log("Rohit">"mohit");
+
+console.log(10>"10");
+console.log(10>=10);
