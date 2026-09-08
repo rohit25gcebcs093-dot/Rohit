4acb08325113c6abf753bc5354055ae2257f3982
Rohit Maddheshiya <rohit.25gcebcs093@galgotiacollege.edu>
2026-09-08 21:05:15 +0530

    Day 03

---
+++ Java Script/Day03.js/Day03.js
@@ -0,0 +1,43 @@
+// for looping
+for(let i=0 ; i<10 ; i++){
+    console.log(i);
+}
+
+//while looping 
+let j=0;
+while(j<10){
+    console.log(j);
+    j++;
+}
+
+//do-while loop
+let k=0;
+do{
+    console.log(k);
+    k++;
+}while(k<10);
+
+//if condition
+let age=15;
+if(age>=18){
+    console.log("Eligible for vote");
+}
+else{
+    console.log("Not eligible for vote");
+}
+
+//Loigcal operartion
+let a="Rohit";
+let b="Mohit";
+let c=a&&b;
+console.log(c);
+
+let d=" ";
+let e="Mohit";
+let f=a&&b;
+console.log(f);
+
+let g=10;
+let h=20;
+console.log(g||h);
+
